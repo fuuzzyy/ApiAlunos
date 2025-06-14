ceb37832c4e3722aa285ecaee42ff1b9e15bee40
Fuuzzyy <0001075915@senaimgaluno.com.br>
2025-06-14 14:10:27 -0300

    first commit

---
+++ src/main/java/com/example/CruddeAlunos/CruddeAlunosApplication.java
@@ -0,0 +1,13 @@
+package com.example.cruddealunos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddeAlunosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddeAlunosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CruddeAlunos/CruddeAlunosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CruddeAlunos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddeAlunosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
